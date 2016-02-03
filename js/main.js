80df65ff190073ece46c07d6d14b826ac3d0df32
michael-dickens <mdickens1393@gmail.com>
2016-02-02 18:56:36 -0600

    Module 2 submission
    Couldn't get the addColumns to work without duplicating ALL of the data
    multiple times

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 //initialize function called when the script loads
 function initialize(){
     cities();
-    addColumns();
     addEvents();
+    addColumns();
 };
 
-//function to create a table with cities and their populations
+//Create a table with cities and their populations
 function cities(){
     //define two arrays for cities and population
     var cityPop = [
@@ -26,7 +26,6 @@ function cities(){
             population: 4879566
         }
     ];
-
     //append the table element to the div
     $("#mydiv").append("<table>");
 
@@ -44,33 +43,28 @@ function cities(){
         $("table").append(rowHtml);
     };
 };
-//function to add columns to a created table, taking cityPop as an argument
-
-
-function addColumns(cityPop){
-    
+//function to add the column citySize to an existing table
+function addColumns(){
     $('tr').each(function(i){
         $("#mydiv").append("<table>");
-        
+        // when i = 0 City Size is added to the header
         if (i == 0){
          $("tr").append('<th>City Size</th>');   
         } else {
-            
             // Classifies cities by adding a new attribute citySize
-
             var citySize;
+            
+            if (cityPop[i-1].population < 100000){
+                citySize = 'Small';
 
-            // if (cityPop[i-1].population < 100000){
-            //     citySize = 'Small';
+            } else if (cityPop[i-1].population < 500000 || cityPop[i-1].population >= 100000){
+                citysize = 'Medium';
 
-            // } else if (cityPop[i-1].population < 500000 || cityPop[i-1].population >= 100000){
-            //     citysize = 'Medium';
-
-            // } else{
-            //     citySize = 'Large';
-            // };
-
-            // $("tr").append('<td>' + citySize + '</td>');
+            } else{
+                citySize = 'Large';
+            };
+            // adds the city size to the table
+            $("tr").append('<td>' + citySize + '</td>');
         };
     });
 };
@@ -104,12 +98,10 @@ function addEvents(){
     function clickme(){
         // the click messge
         alert('Hey, you clicked me!');
-        //prevents the message box from popping up after exiting
-        ('table').off('click', clickme);
     };
     // adding the event listener
     $('table').on('click', clickme);
     });
 };
-//call the initialize function when the document has loaded
+//once the document is done loading start the functions
 $(document).ready(initialize);
